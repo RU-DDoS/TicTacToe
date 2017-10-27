43fcfe3a95629d7510c426edfb8eeeb538adbc20
Miscon <matthiasd16@hr.is>
2017-10-27 14:56:52 +0000

    Didn't work so revert

--- src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
@@ -4,21 +4,23 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.*;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public abstract class SeleniumTestWrapper {
-  static PhantomJSDriver driver;
+  static ChromeDriver driver;
   static String baseUrl;
   static String port;
 
   @BeforeClass
   public static void openBrowser() {
-    //driver = new ChromeDriver(chromeOptions);
-    driver = new PhantomJSDriver();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.setBinary("/tmp/google-chrome-stable_current_amd64.deb");
+    chromeOptions.addArguments("--headless");
+    chromeOptions.addArguments("--disable-gpu");
+    driver = new ChromeDriver(chromeOptions);
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     baseUrl = "https://tictactoe-staging-ddos.herokuapp.com";
