1899a8f9bf36807cb39577c730b05e3a5d1daa8a
Miscon <matthiasd16@hr.is>
2017-10-27 14:43:39 +0000

    New way of doing things test

--- src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.*;
 
 import org.junit.AfterClass;
@@ -16,11 +17,8 @@ public abstract class SeleniumTestWrapper {
 
   @BeforeClass
   public static void openBrowser() {
-    ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.setBinary("/tmp/google-chrome-stable_current_amd64.deb");
-    chromeOptions.addArguments("--headless");
-    chromeOptions.addArguments("--disable-gpu");
     driver = new ChromeDriver(chromeOptions);
+    driver = new PhantomJSDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     baseUrl = "https://tictactoe-staging-ddos.herokuapp.com";
